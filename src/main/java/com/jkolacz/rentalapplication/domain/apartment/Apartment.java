d37a769c39ba0185ff267bd950bedfd8b4e3a81f
j.kolacz <kolacz.jakub2@gmail.com>
2022-01-23 00:25:18 +0100

    Adding new apartment - add required JPA annotation over Apartment and attributes

--- src/main/java/com/jkolacz/rentalapplication/domain/apartment/Apartment.java
+++ src/main/java/com/jkolacz/rentalapplication/domain/apartment/Apartment.java
@@ -1,12 +1,19 @@
 package com.jkolacz.rentalapplication.domain.apartment;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
 public class Apartment {
+    @Id
+    @GeneratedValue
     private String id;
     private final String ownerId;
+
+    @Embedded
     private final Address address;
     private final String description;
+    @OneToMany
     private final List<Room> rooms;
 
     Apartment(String ownerId, Address address, String description, List<Room> rooms) {
--- src/main/java/com/jkolacz/rentalapplication/domain/apartment/Room.java
+++ src/main/java/com/jkolacz/rentalapplication/domain/apartment/Room.java
@@ -1,7 +1,15 @@
 package com.jkolacz.rentalapplication.domain.apartment;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 class Room {
+    @Id
+    private String id;
     private final String name;
+    @Embedded
     private final SquareMeter squareMeter;
 
     Room(String name, SquareMeter squareMeter) {
