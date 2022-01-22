e8fd9717b3ea029567ee944ef7f239d4ff68869f
j.kolacz <kolacz.jakub2@gmail.com>
2022-01-23 00:21:12 +0100

    Adding new apartment - introduce JpaApartmentRepository to fulfill ApartmentRepository contract

---
+++ src/main/java/com/jkolacz/rentalapplication/infrastructure/persistence/jpa/apartment/ApartmentJpaRepository.java
@@ -0,0 +1,18 @@
+package com.jkolacz.rentalapplication.infrastructure.persistence.jpa.apartment;
+
+import com.jkolacz.rentalapplication.domain.apartment.Apartment;
+import com.jkolacz.rentalapplication.domain.apartment.ApartmentRepository;
+
+public class ApartmentJpaRepository implements ApartmentRepository {
+
+    private final SpringApartmentJpaRepository springApartmentJpaRepository;
+
+    public ApartmentJpaRepository(SpringApartmentJpaRepository springApartmentJpaRepository) {
+        this.springApartmentJpaRepository = springApartmentJpaRepository;
+    }
+
+    @Override
+    public void save(Apartment apartment) {
+        springApartmentJpaRepository.save(apartment);
+    }
+}
---
+++ src/main/java/com/jkolacz/rentalapplication/infrastructure/persistence/jpa/apartment/SpringApartmentJpaRepository.java
@@ -0,0 +1,7 @@
+package com.jkolacz.rentalapplication.infrastructure.persistence.jpa.apartment;
+
+import com.jkolacz.rentalapplication.domain.apartment.Apartment;
+import org.springframework.data.repository.CrudRepository;
+
+interface SpringApartmentJpaRepository extends CrudRepository<Apartment, String> {
+}
