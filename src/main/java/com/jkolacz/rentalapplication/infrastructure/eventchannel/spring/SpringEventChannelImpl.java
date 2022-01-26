cf062668b09d0afe1a966939c0b2ac4e4324e352
j.kolacz <kolacz.jakub2@gmail.com>
2022-01-26 23:32:03 +0100

    Add history about apartment booking - add implementation of Event Channel

---
+++ src/main/java/com/jkolacz/rentalapplication/infrastructure/eventchannel/spring/SpringEventChannelImpl.java
@@ -0,0 +1,26 @@
+package com.jkolacz.rentalapplication.infrastructure.eventchannel.spring;
+
+import com.jkolacz.rentalapplication.domain.apartment.ApartmentBooked;
+import com.jkolacz.rentalapplication.domain.eventchannel.EventChannel;
+import com.jkolacz.rentalapplication.domain.hotelRoom.HotelRoomBooked;
+import org.springframework.context.ApplicationEventPublisher;
+
+class SpringEventChannelImpl implements EventChannel {
+
+    private final ApplicationEventPublisher publisher;
+
+    SpringEventChannelImpl(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public void publish(ApartmentBooked apartmentBooked) {
+        publisher.publishEvent(apartmentBooked);
+
+    }
+
+    @Override
+    public void publish(HotelRoomBooked hotelRoomBooked) {
+        publisher.publishEvent(hotelRoomBooked);
+    }
+}
