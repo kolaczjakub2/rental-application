bebb9e096486d7125431849c2961311d142f7e28
j.kolacz <kolacz.jakub2@gmail.com>
2022-02-08 23:37:35 +0100

    Unit Tests - add implementation of assertion

--- src/test/java/com/jkolacz/rentalapplication/domain/apartment/ApartmentTest.java
+++ src/test/java/com/jkolacz/rentalapplication/domain/apartment/ApartmentTest.java
@@ -3,7 +3,10 @@ package com.jkolacz.rentalapplication.domain.apartment;
 import com.google.common.collect.ImmutableMap;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ApartmentTest {
 
@@ -32,19 +35,35 @@ class ApartmentTest {
     }
 
     private void assertThatHasOwnerId(Apartment actual, String ownerId) {
-
+        assertThat(actual).hasFieldOrPropertyWithValue("ownerId", ownerId);
     }
 
     private void assertThatHasDescription(Apartment actual, String description) {
-
+        assertThat(actual).hasFieldOrPropertyWithValue("description", description);
     }
 
     private void assertThatHasAddress(Apartment actual, String street, String postalCode, String houseNumber, String apartmentNumber, String city, String country) {
-
+        assertThat(actual).extracting("address")
+                .hasFieldOrPropertyWithValue("street", street)
+                .hasFieldOrPropertyWithValue("postalCode", postalCode)
+                .hasFieldOrPropertyWithValue("houseNumber", houseNumber)
+                .hasFieldOrPropertyWithValue("apartmentNumber", apartmentNumber)
+                .hasFieldOrPropertyWithValue("city", city)
+                .hasFieldOrPropertyWithValue("country", country);
     }
 
     private void assertThatHasRooms(Apartment actual, Map<String, Double> roomDefinition) {
-
+        assertThat(actual).extracting("rooms").satisfies(roomsActual -> {
+            List<Room> rooms = (List<Room>) roomsActual;
+            assertThat(rooms).hasSize(roomDefinition.size());
+            roomDefinition.forEach((name, squareMeter) -> {
+                assertThat(rooms).anySatisfy(room -> {
+                    assertThat(room)
+                            .hasFieldOrPropertyWithValue("name", name)
+                            .hasFieldOrPropertyWithValue("squareMeter.size", squareMeter);
+                });
+            });
+        });
     }
 
 }
