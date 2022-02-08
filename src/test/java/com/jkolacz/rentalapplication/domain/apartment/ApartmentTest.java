d3462ca6e31518ba0d0e87a3d47d5f6b192c0760
j.kolacz <kolacz.jakub2@gmail.com>
2022-02-08 23:24:05 +0100

    Unit Tests - inline variables

---
+++ src/test/java/com/jkolacz/rentalapplication/domain/apartment/ApartmentTest.java
@@ -0,0 +1,50 @@
+package com.jkolacz.rentalapplication.domain.apartment;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+class ApartmentTest {
+
+    @Test
+    public void shouldCreateApartmentWithRequiredFields() {
+        String ownerId = "1234";
+        String street = "Florianska";
+        String postalCode = "12-345";
+        String houseNumber = "1";
+        String apartmentNumber = "13";
+        String city = "Cracow";
+        String country = "Poland";
+        String description = "Nice place to stay";
+        Map<String, Double> roomDefinition = ImmutableMap.of(
+                "Toilet", 10.0, "Bedroom", 30.0
+        );
+        Apartment actual = new ApartmentFactory().create(
+                ownerId, street, postalCode, houseNumber, apartmentNumber, city, country,
+                description, roomDefinition);
+
+        assertThatHasOwnerId(actual, ownerId);
+        assertThatHasDescription(actual, description);
+        assertThatHasAddress(actual, street, postalCode, houseNumber, apartmentNumber, city, country);
+        assertThatHasRooms(actual, roomDefinition);
+
+    }
+
+    private void assertThatHasOwnerId(Apartment actual, String ownerId) {
+
+    }
+
+    private void assertThatHasDescription(Apartment actual, String description) {
+
+    }
+
+    private void assertThatHasAddress(Apartment actual, String street, String postalCode, String houseNumber, String apartmentNumber, String city, String country) {
+
+    }
+
+    private void assertThatHasRooms(Apartment actual, Map<String, Double> roomDefinition) {
+
+    }
+
+}
