76c2713878632e350a009ba78fd82dd4e4a399e4
j.kolacz <kolacz.jakub2@gmail.com>
2022-05-26 17:00:44 +0200

    Introduce lombok

--- src/main/java/com/jkolacz/rentalapplication/infrastructure/rest/api/hotel/HotelDto.java
+++ src/main/java/com/jkolacz/rentalapplication/infrastructure/rest/api/hotel/HotelDto.java
@@ -1,5 +1,10 @@
 package com.jkolacz.rentalapplication.infrastructure.rest.api.hotel;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class HotelDto {
     private final String name;
     private final String street;
@@ -7,37 +12,4 @@ public class HotelDto {
     private final String postalCode;
     private final String city;
     private final String country;
-
-    public HotelDto(String name, String street, String buildingNumber, String postalCode, String city, String country) {
-        this.name = name;
-        this.street = street;
-        this.buildingNumber = buildingNumber;
-        this.postalCode = postalCode;
-        this.city = city;
-        this.country = country;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public String getBuildingNumber() {
-        return buildingNumber;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
 }
