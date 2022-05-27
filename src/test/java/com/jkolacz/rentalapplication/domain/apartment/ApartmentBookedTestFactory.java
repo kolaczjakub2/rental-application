021845f5835e4089bd8a343329ed009443131c89
j.kolacz <kolacz.jakub2@gmail.com>
2022-05-27 13:49:18 +0200

    ApartmentBooked Refactor

--- src/test/java/com/jkolacz/rentalapplication/domain/apartment/ApartmentBookedTestFactory.java
+++ src/test/java/com/jkolacz/rentalapplication/domain/apartment/ApartmentBookedTestFactory.java
@@ -2,7 +2,7 @@ package com.jkolacz.rentalapplication.domain.apartment;
 
 public class ApartmentBookedTestFactory {
 
-    public static ApartmentBooked create(String eventId,String apartmentId, String ownerId, String tenantId, Period period, ) {
+    public static ApartmentBooked create(String eventId,String apartmentId, String ownerId, String tenantId, Period period) {
         return ApartmentBooked.create(eventId,apartmentId, ownerId, tenantId, period);
     }
 }
